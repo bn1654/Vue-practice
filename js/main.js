73f748c2eb99afa89a2869403707ed371b0143ed
bn1654 <stikenloni@gmail.com>
2026-03-02 13:48:52 +0700

    data binding

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@ let app = new Vue({
    el: '#app',
    data: {
        product: "Socks",
-       description: "A pair warm, fuzzy socks"
+       description: "A pair warm, fuzzy socks",
+       image: "./assets/vmSocks-green-onWhite.jpg",
+       altText: "A pair of socks",
+       link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
    }
 })
 
