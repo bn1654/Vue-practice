e39a2928b9dcd3417e6915ad884fb270a2f0dffa
bn1654 <stikenloni@gmail.com>
2026-03-02 13:59:43 +0700

    conditional rendering

--- js/main.js
+++ js/main.js
@@ -5,7 +5,9 @@ let app = new Vue({
        description: "A pair warm, fuzzy socks",
        image: "./assets/vmSocks-green-onWhite.jpg",
        altText: "A pair of socks",
-       link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+       link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+       inventory: 100,
+       onSale: true
    }
 })
 
