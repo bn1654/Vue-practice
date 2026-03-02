be0a3e6f7308a0b7c7630c47840e0be4586145f3
bn1654 <stikenloni@gmail.com>
2026-03-02 14:36:31 +0700

    class connections

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ let app = new Vue({
        image: "./assets/vmSocks-green-onWhite.jpg",
        altText: "A pair of socks",
        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-       inventory: 100,
+       inventory: 15,
        onSale: true,
        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
        variants: [
@@ -28,10 +28,14 @@ let app = new Vue({
    },
    methods: {
         addToCart() {
-            this.cart += 1
+            this.cart += 1;
+            this.inventory -= 1
         },
         deleteFromCart(){
-            if(this.cart > 0) this.cart -= 1
+            if(this.cart > 0){
+                this.cart -= 1;
+                this.inventory += 1
+            }
         },
         updateProduct(variantImage) {
             this.image = variantImage
